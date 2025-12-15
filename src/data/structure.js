569bea53a5b36ba7fc2c3cd7846b90d3aa35d7f9
ViraaShop001 <ranggaraditiawan9@gmail.com>
2025-12-15 15:17:12 +0700

    Add structure.js with class roles and names

---
+++ src/data/structure.js
@@ -0,0 +1,6 @@
+export const structure = [
+  { role: "Ketua Kelas", name: "Arip" },
+  { role: "Wakil Ketua", name: "Ipa" },
+  { role: "Sekretaris", name: "Nita" },
+  { role: "Bendahara", name: "Vani" }
+]
