07a338c35edafee5954d4b4d62aba63c60a039c3
ViraaShop001 <ranggaraditiawan9@gmail.com>
2025-12-15 15:12:55 +0700

    Add Footer component with copyright notice

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 text-center text-gray-400 text-sm border-t border-white/10">
+      © 2025 Astrogenix MIPA 2
+    </footer>
+  )
+}
